3f9546e6f884f7af8819c3c5c1390a77f236ffd0
Omnia Elmenshawy <77496383+omniaelmenshawy@users.noreply.github.com>
2021-09-27 13:48:44 +0200

    Intersection Over Union for Object Detection Accuracy

--- iou.py
+++ iou.py
@@ -1,35 +1,31 @@
+from utils import get_data, check_results
 import numpy as np
 
-from utils import get_data, check_results
+
+
+def calculate_iou(gt_bbox, pred_bbox):
+    
+
+    xmax = np.min([gt_bbox[2], pred_bbox[2]])
+    ymax = np.min([gt_bbox[3], pred_bbox[3]])
+    xmin = np.max([gt_bbox[0], pred_bbox[0]])
+    ymin = np.max([gt_bbox[1], pred_bbox[1]])
+    
+    intersection = max(0, xmax - xmin) * max(0, ymax - ymin)
+    gt_area = (gt_bbox[2] - gt_bbox[0]) * (gt_bbox[3] - gt_bbox[1])
+    pred_area = (pred_bbox[2] - pred_bbox[0]) * (pred_bbox[3] - pred_bbox[1])
+    
+    union = gt_area + pred_area - intersection
+    return intersection / union
 
 
 def calculate_ious(gt_bboxes, pred_bboxes):
-    """
-    calculate ious between 2 sets of bboxes 
-    args:
-    - gt_bboxes [array]: Nx4 ground truth array
-    - pred_bboxes [array]: Mx4 pred array
-    returns:
-    - iou [array]: NxM array of ious
-    """
+
     ious = np.zeros((gt_bboxes.shape[0], pred_bboxes.shape[0]))
     for i, gt_bbox in enumerate(gt_bboxes):
         for j, pred_bbox in enumerate(pred_bboxes):
             ious[i,j] = calculate_iou(gt_bbox, pred_bbox)
     return ious
-
-
-def calculate_iou(gt_bbox, pred_bbox):
-    """
-    calculate iou 
-    args:
-    - gt_bbox [array]: 1x4 single gt bbox
-    - pred_bbox [array]: 1x4 single pred bbox
-    returns:
-    - iou [float]: iou between 2 bboxes
-    """
-    ## IMPLEMENT THIS FUNCTION
-    return iou
 
 
 if __name__ == "__main__": 
